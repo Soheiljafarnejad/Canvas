935983a9502c959c0b39008b06e3f79fc1d4a349
Soheil <jafarnejad.official@gmail.com>
2023-04-25 20:47:31 +0330

    fillRect & clearRect & fillStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { useEffect, useRef } from "react";
 
 const App = () => {
-  const ctx = useRef(null);
+  const canvas = useRef(null);
 
   useEffect(() => {
-    ctx.current = document.getElementById("my-canvas").getContext("2d");
-    console.log(ctx.current);
+    if (canvas.current.getContext) {
+      const ctx = canvas.current.getContext("2d");
+      ctx.fillStyle = "red";
+      ctx.fillRect(10, 10, 50, 50);
+      ctx.clearRect(20, 20, 30, 30);
+
+      ctx.fillStyle = "#fff";
+      ctx.fillRect(150, 150, 30, 30);
+    }
   }, []);
 
-  return <canvas id="my-canvas"></canvas>;
+  return (
+    <div className="flex items-center justify-center min-h-[100svh]">
+      <canvas className="bg-gray-300" width={500} height={500} ref={canvas}></canvas>;
+    </div>
+  );
 };
 
 export default App;
